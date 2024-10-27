a49e65293662ac91a1e21b300b5aa380e040e8a2
akiracopeland <akiracopeland713@gmail.com>
2024-10-27 03:06:11 -0700

    quiz done

--- src/com/amazon/ata/statics/StringValidator.java
+++ src/com/amazon/ata/statics/StringValidator.java
@@ -1,9 +1,15 @@
 package com.amazon.ata.statics;
 
+
+import java.util.List;
+
 public final class StringValidator {
 
     // Create a private constructor so StringValidator objects cannot be created.
     private StringValidator() {}
+
+    private static final String PROHIBITED_WORD = "fish";
+    private static final int MAX_LENGTH = 250;
 
     /**
      * Checks to see if the string entered contains a forbidden string.
@@ -12,7 +18,16 @@ public final class StringValidator {
      * @throws InvalidStringException if the input contains a forbidden string
      */
     public static void containsForbiddenWord(String input) throws InvalidStringException {
-        throw new InvalidStringException("The given String contains a forbidden word.");
+
+        List<String> words = List.of(input.split("\\s+"));
+
+        for (String word : words) {
+            if (word.equals(PROHIBITED_WORD)) {
+                throw new InvalidStringException("The given String contains a forbidden word.");
+            }
+        }
+
+
     }
 
     /**
@@ -22,6 +37,9 @@ public final class StringValidator {
      * @throws InvalidStringException checks to see if the input is a forbidden string
      */
     public static void greaterThanMaxLength(String input) throws InvalidStringException {
-        throw new InvalidStringException("The given String has a length greater than the max length allowed.");
+
+        if (input.length() > MAX_LENGTH) {
+            throw new InvalidStringException("The given String has a length greater than the max length allowed.");
+        }
     }
 }
